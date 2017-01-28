6b9aad2a5b729e721f6078bce961d0eceed8333b
Jose Marquez <joseangel2212@gmail.com>
2017-01-28 16:58:38 -0400

    update (pages) : updated Layout page, added material ui

--- client/containers/Layout.js
+++ client/containers/Layout.js
@@ -1,15 +1,26 @@
 /**
  * Created by joseangelmr on 28/1/17.
  */
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import {SelectField, RaisedButton, AppBar, MenuItem, FontIcon} from 'material-ui/';
+import {List, ListItem} from 'material-ui/List';
+
 
 export default class Layout extends React.Component {
     render() {
         return (
-            <div>
-                <h4>Layout</h4>
-                { this.props.children }
-            </div>
+            <MuiThemeProvider muiTheme={ getMuiTheme(darkBaseTheme) }>
+                <div>
+                    <AppBar title="Github App" showMenuIconButton={ false }/>
+
+                    { this.props.children }
+
+                </div>
+
+            </MuiThemeProvider>
         );
     }
 }
