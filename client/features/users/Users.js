0d064dd3706dcda9644f223932b04f6827136653
Jose Marquez <joseangel2212@gmail.com>
2017-01-28 16:59:24 -0400

    feat (pages): updated Users page, added users list

--- client/features/users/Users.js
+++ client/features/users/Users.js
@@ -2,8 +2,11 @@
  * Created by joseangelmr on 28/1/17.
  */
 import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from '../../../commons/redux/actions';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import { blue500, yellow600 } from 'material-ui/styles/colors';
 
 export class Users extends Component {
 
@@ -11,10 +14,41 @@ export class Users extends Component {
         super(props);
     }
 
+    componentWillMount(){
+        this.props.getUsers()
+    }
+
+    renderUsers() {
+
+        if (this.props.users.length > 0) {
+            return <List>
+
+                {
+                    this.props.users.map((user, i) => {
+
+                        return (
+                            <ListItem
+                                leftAvatar={<Avatar src={ user.avatar_url } />}
+                                primaryText={ user.login }
+                            />
+                        )
+                    })
+                }
+                
+            </List>
+        }
+
+        return ''
+
+    }
+
     render() {
+
+        const users = this.renderUsers()
+
         return (
             <div className="users-default-page">
-                Component content: users/Users
+                { users }
             </div>
         );
     }
